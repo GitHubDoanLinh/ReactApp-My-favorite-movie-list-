b8bf64d2f886fc5c3a31d88caeae324cef9b53e2
Doan Huu Linh <doanlinh1510@gmail.com>
2024-08-06 00:47:13 +0700

    commit 08 Improving Reusability With Props

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating color="lightgreen" size={24} className="test" defaultRating={3}/>
   </React.StrictMode>
 );
